2c24cddcf87e58c78df7c14a0af5629d458b2a18
Michail Christoforatos <mchristoforatos@icloud.com>
2024-10-21 14:19:44 +0300

    added socials in blog posts

--- app/b/[slug]/page.js
+++ app/b/[slug]/page.js
@@ -3,6 +3,7 @@ import path from "path";
 import { readFile, access } from "fs/promises";
 import { notFound } from "next/navigation";
 import MdxLayout from "@/components/mdx-layout";
+import Socials from "@/components/socials";
 
 const POSTS_FOLDER = path.join(process.cwd(), "posts");
 
@@ -32,18 +33,19 @@ export default async function PostPage({ params }) {
   });
 
   return (
-    <div className="flex justify-center min-h-screen">
-      <div className="max-w-2xl w-full mx-2 sm:mx-5">
+    <div className="flex flex-col items-center min-h-screen">
+      <div className="max-w-2xl w-full mx-2 sm:mx-5 flex-grow">
         <div className="mt-14">
           <article>
             <h1 className="leading-4 font-medium">{frontmatter.title}</h1>
             <p className="text-[#d9d9d9] font-light text-[15.5px]">Michail Christoforatos</p>
-            <div className="mt-10">
+            <div className="my-10">
               <MdxLayout>{content}</MdxLayout>
             </div>
           </article>
         </div>
       </div>
+      <Socials />
     </div>
   );
 }
