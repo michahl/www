856655e8019f0eddf6279937b6ce7afd6344a591
Michail Christoforatos <mchristoforatos@icloud.com>
2025-02-25 20:52:29 +0200

    add support for mdx files

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
+import withMDX from "@next/mdx";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default withMDX({
+  extension: /\.mdx?$/
+})(nextConfig);
