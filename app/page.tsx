2ecbdf0757089851b54a67505e09673de96e874f
Michail Christoforatos <mchristoforatos@icloud.com>
2025-03-18 15:51:54 +0200

    update height setting for home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Socials from "@/components/socials";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col justfiy-between pt-0 md:pt-8 p-2 md:p-8">
+    <div className="h-dvh flex flex-col justfiy-between pt-0 md:pt-8 p-2 md:p-8">
       <main className="max-w-[38rem] mx-auto w-full space-y-6 flex-grow">
         <h1 className="mx-3.5 pt-12">Michail Christoforatos</h1>
         <p className="mx-3.5">
