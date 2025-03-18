a218189f1967472a3b6f7d86c41d265d1981a49a
Michail Christoforatos <mchristoforatos@icloud.com>
2025-03-18 15:22:38 +0200

    refactor social links component for improved layout and styling

--- components/socials.tsx
+++ components/socials.tsx
@@ -2,26 +2,21 @@ import { socialLinks } from "@/lib/constants";
 
 export default function Socials() {
     return (
-        <div className="flex justify-center">
-            <div className="mb-5">
-                <div className="flex">
-                {
-                    socialLinks.map((social, index) => (
-                    <a 
-                        key={index} 
-                        target={social.target}
-                        href={social.href}
-                        className={`
-                            text-[15.75px] text-[#d9d9d9e9] hover:text-[#f9f8fc] transition-colors
-                            flex items-center justify-center p-1   
-                        `}
-                    >
-                        <p className="p-3">{social.title}</p>
-                    </a>
-                    ))
-                }
-                </div>
+        <footer className="mt-12 text-center">
+            <div className="flex justify-center space-x-4 tracking-tight">
+            {
+                socialLinks.map((social, index) => (
+                <a 
+                    key={index} 
+                    target={social.target}
+                    href={social.href}
+                    className="p-1 font-light text-[#d9d9d9e9] hover:text-[#f9f8fc] transition-colors duration-150"
+                >
+                    <p className="p-3">{social.title}</p>
+                </a>
+                ))
+            }
             </div>
-        </div>
+        </footer>
     )
 }
